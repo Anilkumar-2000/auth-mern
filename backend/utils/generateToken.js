20f098cca9696e038a00c6246246fb42b8029d95
Anilkumar-2000 <anilkml682@gmail.com>
2026-01-03 16:33:32 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateToken = (res, userId) => {
     secure: process.env.JWT_SECRET === "production", 
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-  }
+  })
 
   return token;
 
