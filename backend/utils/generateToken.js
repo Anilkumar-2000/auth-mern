d15ed8d9080f3cd989ec9f37c48d5db26f9a3cc2
Anilkumar-2000 <anilkml682@gmail.com>
2026-01-03 16:32:10 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ export const generateToken = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.JWT_SECRET === "production", 
-    sameSite: secure: process.env.NODE_ENV === "production" ? "none" : "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   }
 
